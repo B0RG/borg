6bd792558f57e838a81820cb55ee4b80d902225e
Tejas Ravindra Mandke <tejas@identified.com>
2013-03-07 21:24:54 -0800

    move to the ned directory structure

--- lib/borg/assimilator.rb
+++ lib/borg/assimilator.rb
@@ -3,10 +3,10 @@ module Borg
     def assimilate gem_name
       gem_home = Gem::Specification.find_by_name(gem_name).gem_dir
       Capistrano::Configuration.instance.load do
-        Dir["#{gem_home}/collective/initializers/**/*.rb"].each do |file|
+        Dir["#{gem_home}/cap/initializers/**/*.rb"].each do |file|
           load file
         end
-        @load_paths << "#{gem_home}/collective"
+        @load_paths << "#{gem_home}/cap"
       end
     end
   end
