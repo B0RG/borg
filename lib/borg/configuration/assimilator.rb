4896aed15c04a263a9f27da77371dbad11bf6078
Tejas Ravindra Mandke <tejas@identified.com>
2013-03-11 22:19:58 -0700

    move assimilator to better represent where its pluged into capistrano

---
+++ lib/borg/configuration/assimilator.rb
@@ -0,0 +1,14 @@
+module Borg
+  module Configuration
+    module Assimilator
+      def assimilate gem_name
+        gem_home = Gem::Specification.find_by_name(gem_name).gem_dir
+        Dir["#{gem_home}/cap/initializers/**/*.rb"].each do |file|
+          load file
+        end
+        @load_paths << "#{gem_home}/cap"
+      end
+    end
+  end
+end
+Capistrano::Configuration.send :include, Borg::Configuration::Assimilator
