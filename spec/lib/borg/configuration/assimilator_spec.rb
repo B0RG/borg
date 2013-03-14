161ef11ad1c0402545436529d7a01e708a025ad6
Tejas Ravindra Mandke <tejas@identified.com>
2013-03-14 05:17:00 -0700

    add assimilator tests

---
+++ spec/lib/borg/configuration/assimilator_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+require 'borg/configuration/assimilator'
+
+describe Borg::Configuration::Assimilator do
+  it "should be included into Capistrano::Configuration" do
+    Capistrano::Configuration.new.should respond_to :assimilate
+  end
+
+  context "when assimilate('borg') is called" do
+    subject { Capistrano::Configuration.new }
+    it "should loads all the initializers" do
+      Dir["cap/initializers/**/*.rb"].each do |file|
+        subject.should_receive(:load).with(File.expand_path(file))
+      end
+      subject.assimilate ('borg')
+    end
+
+    it "should add the cap directory to teh load path" do
+      Dir.stub("[]").and_return([])
+      lambda { subject.assimilate ('borg') }.should change(subject.load_paths, :count).by 1
+    end
+  end
+end
