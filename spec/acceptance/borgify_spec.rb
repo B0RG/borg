cdf8777d22d1169b5d088f58127989b982a2f7c1
Kevin Tham <kevinsf90@gmail.com>
2013-03-22 16:43:22 -0700

    Fix deprecated use of let in before(:all)

--- spec/acceptance/borgify_spec.rb
+++ spec/acceptance/borgify_spec.rb
@@ -1,31 +1,29 @@
 require 'acceptance_spec_helper'
 
-describe "borgify (in /bin/borgify)" do
+describe "borgify" do
   include_context "acceptance"
 
-  before(:all) do
+  before do
     assert_execute("borgify")
     @workdir = environment.workdir_path
   end
 
-  it "creates a Gemfile that sources rubygems.org and contains borg-rb" do
+  it "creates the right files and directories" do
+    # Gemfile
     gemfile = @workdir.join("Gemfile")
     expect(gemfile.exist?).to be_true
     expect(gemfile.read).to match(/^source "https:\/\/rubygems.org"$/)
     expect(gemfile.read).to match(/borg-rb/)
-  end
 
-  it "creates a Capfile" do
+    # Capfile
     capfile = @workdir.join("Capfile")
     expect(capfile.exist?).to be_true
-  end
 
-  it "creates a lib directory" do
-    capfile = @workdir.join("Capfile")
+    # lib directory
+    capfile = @workdir.join("lib")
     expect(capfile.exist?).to be_true
-  end
 
-  it "create a cap directory with the following subdirectories: applications, initializers, recipes" do
+    # cap directory with the subdirectories: applications, initializers, recipes
     cap_dir = @workdir.join("cap")
     expect(cap_dir.exist?).to be_true
     expect(cap_dir.join("applications")).to be_true
@@ -37,12 +35,13 @@ end
 describe "borgify plugin" do
   include_context "acceptance"
 
-  before(:all) do
+  before do
     assert_execute("borgify", "plugin")
     @workdir = environment.workdir_path
   end
 
-  it "creates a gemspec" do
+  it "creates the right files and directories" do
+    # the gemspec
     directory_name = File.basename(@workdir = environment.workdir_path)
     gemspec = @workdir = environment.workdir_path.join("#{directory_name}.gemspec")
     expect(gemspec.exist?).to be_true
